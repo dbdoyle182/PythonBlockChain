c5cff3ece9aca2cbf7c9e962dabc14384c89360b
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-19 19:37:55 -0400

    completed block class and testing the class out

--- block.py
+++ block.py
@@ -27,4 +27,17 @@ def next_block(last_block):
     this_hash = last_block.hash
     return Block(this_index, this_timestamp, this_data, this_hash)
 
-    
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+num_of_blocks_to_add = 20
+
+for i in range(0, num_of_blocks_to_add):
+    block_to_add = next_block(previous_block)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+
+    print(f"Block {block_to_add.index} has been added to the blockchain!")
+    print(f"Hash: {block_to_add.hash} \n")
+
+
